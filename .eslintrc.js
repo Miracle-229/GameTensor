6d4a052dbde786a6d45ad09973d7ffb5f9be97f2
Miracle-229 <zzzzhd02@gmail.com>
2023-12-15 13:02:50 +0300

    start project

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-props-no-spreading': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  // overrides: [
+  //   {
+  //     files: ['/src/api/firebase-config.js'],
+  //     rules: {
+  //       'import/prefer-default-export': 'off', // Отключение правила для данного файла
+  //     },
+  //   },
+  // ],
+};
