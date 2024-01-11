1c976a6b26ae3ac26b71c2cdaccf8586bf62ac52
Miracle-229 <zzzzhd02@gmail.com>
2024-01-11 14:12:12 +0300

    change config for env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const fs = require('fs');
-const dotenv = require('dotenv').config({ silent: true });
+const dotenv = require('dotenv');
 
 const env = dotenv.parse(fs.readFileSync('.env.local'));
 const nextConfig = {
